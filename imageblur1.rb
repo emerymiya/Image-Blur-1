ee575cb63fad3b47e15c7ce9834653df3d772c5b
Emery Green <emerymiya@gmail.com>
2019-12-25 16:34:52 +0000

    Image Blur 3 complete

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,58 +1,61 @@
 class Image
+  attr_accessor :array
 
-  def initialize (array)
+  def initialize(array)
     @array = array
   end
 
-  def get_ones
-    ones = []
-    @array.each_with_index do |row, r|
-      row.each_with_index do |value, c|
-        if value == 1
-          ones << [r, c]
-        end
-      end
+  def blur(distance = 1)
+    distance.times do
+      transform
     end
-    ones
-  end
-
-  def distance!
-    ones = get_ones
-      @array.each_with_index do |row, r|
-        row.each_with_index do |value, c|
-          ones.each do |one_r, one_c|
-
-            if r == one_r && c == one_c
-              @array[r -1][c] = 1 unless r == 0
-              @array[r +1][c] = 1 unless r >= 3 
-              @array[r][c -1] = 1 unless c == 0 
-              @array[r][c +1] = 1 unless c >= 3 
-              @array[r -2][c] = 1 unless r <= 1
-              @array[r +2][c] = 1 unless r >= 2
-              @array[r][c -2] = 1 unless c <= 1
-              @array[r][c +2] = 1 unless c >= 2
-            end
-          end
-        end
-      end
   end
 
   def output_image
-    @array.each do |data|
-      puts data.join
+    @array.each {|item| puts "#{item.join()}"}
+  end
+
+  private
+
+  def transform
+    width = array[0].length
+
+    height = array.length
+
+    ones_array = []
+
+    array.each_with_index do |row, r|
+      row.each_with_index do |value, c|
+        ones_array << [r, c] if value == 1
+      end
+    end
+
+    ones_array.each do |coordinate|
+      r, c = coordinate
+
+      array[c][r + 1] = 1 unless (r + 1 >= width)
+      array[c][r - 1] = 1 unless (r - 1 < 0)
+      array[c + 1][r] = 1 unless (c + 1 >= height)
+      array[c - 1][r] = 1 unless (c - 1 < 0)
     end
   end
 end
 
+
 image = Image.new([
-[0, 0, 0, 0],
-[0, 1, 0, 0],
-[0, 0, 0, 1],
-[0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ])
 
-
-image.distance!
+image.blur(3)
 image.output_image
 
 
